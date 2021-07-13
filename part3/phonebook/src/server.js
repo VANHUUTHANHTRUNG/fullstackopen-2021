64811cd11b3b9ddd766f98f7088e917355c5f95a
trung <vanhuuthanhtrung@gmail.com>
2021-07-13 09:02:05 +0300

    add 3c

--- part3/phonebook/src/server.js
+++ part3/phonebook/src/server.js
@@ -1,39 +1,28 @@
+require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+const Person = require("./models/Person");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-let persons = [
-    {
-        id: 1,
-        name: "Arto Hellas",
-        number: "040-123456",
-    },
-    {
-        id: 2,
-        name: "Ada Lovelace",
-        number: "39-44-5323523",
-    },
-    {
-        id: 3,
-        name: "Dan Abramov",
-        number: "12-43-234345",
-    },
-    {
-        id: 4,
-        name: "Mary Poppendieck",
-        number: "39-23-6423122",
-    },
-];
-
-const generateID = (min, max) => {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-};
-
 const unknownEndpoint = (_req, res) =>
     res.status(404).send({ error: "Unknown endpoint" });
+
+const errorHandler = (error, _req, res, next) => {
+    console.log(error.message);
+
+    if (error.name === "CastError" && error.kind === "ObjectId") {
+        return res
+            .status(400)
+            .json({ error: "Malformatted id: Record with ID does not exist." });
+    }
+    if (error.name === "ValidationError") {
+        return res.status(400).json({ error: error.message });
+    }
+    next(error);
+};
 
 morgan.token("post-param", (req, _res) => {
     const body = req.body;
@@ -53,51 +42,66 @@ app.use(
 );
 
 app.get("/info", (_req, res) => {
-    let content = "";
-    content += `<p>Phone has info of ${persons.length} people</p>`;
-    content += `<p>${new Date().toString()}</p>`;
-    res.end(content);
+    Person.find({}).then((persons) => {
+        let content = "";
+        content += `<p>Phone has info of ${persons.length} people</p>`;
+        content += `<p>${new Date().toString()}</p>`;
+        res.end(content);
+    });
 });
 
 app.get("/api/persons", (_req, res) => {
-    res.json(persons);
+    Person.find({}).then((persons) => res.json(persons));
 });
 
-app.get("/api/persons/:id", (req, res) => {
-    const id = Number(req.params.id);
-    const foundPerson = persons.find((person) => person.id === id);
-    if (foundPerson !== undefined) res.json(foundPerson);
-    else res.status(404).end();
+app.get("/api/persons/:id", (req, res, next) => {
+    Person.findById(req.params.id)
+        .then((person) => {
+            if (person) {
+                res.json(person);
+            } else {
+                res.status(404).send().end();
+            }
+        })
+        .catch((error) => next(error));
 });
 
-app.post("/api/persons/", (req, res) => {
+app.post("/api/persons/", (req, res,next) => {
     const body = req.body;
     if (!body.name || !body.number)
         return res.status(400).json({ error: "Name or number missing" });
-    const id = generateID(1, 1000);
-    sameIdPerson = persons.find((person) => person.id === id);
-    if (sameIdPerson !== undefined)
-        return res
-            .status(400)
-            .json({ erorr: `generated ID: ${id} has been taken, try again` });
-    sameNamePerson = persons.find((person) => person.name === body.name);
-    if (sameNamePerson !== undefined)
-        return res.status(400).json({
-            error: `name must be unique, ${body.name} is already in phonebook`,
-        });
-    persons = [...persons, { id: id, name: body.name, number: body.number }];
-    res.status(201).end(`Successfully added ${body.name}`);
+
+    const newPerson = new Person({
+        name: body.name,
+        number: body.number,
+    });
+    newPerson.save().then((savedPerson) => res.json(savedPerson))
+    .catch(error => next(error));
 });
 
-app.delete("/api/persons/:id", (req, res) => {
-    const id = Number(req.params.id);
-    const foundPerson = persons.find((person) => person.id === id);
-    if (foundPerson !== undefined) {
-        persons = persons.filter((person) => person.id !== id);
-        res.end(`Deleted ${foundPerson.name} from phonebook`);
-    } else res.status(404).end("Can not find person to delete");
+app.put("/api/persons/:id", (req, res,next) => {
+    const body = req.body;
+    const person = new Person({
+        _id: req.params.id,
+        name: body.name,
+        number: body.number,
+    });
+    Person.findByIdAndUpdate({_id: req.params.id}, person, {new: true})
+    .then(updatePerson => res.status(204).json(updatePerson))
+    .catch(error => next(error))
+});
+
+app.delete("/api/persons/:id", (req, res, next) => {
+    Person.findByIdAndRemove(req.params.id)
+        .then((result) => {
+            if(result) res.status(204).json(result).end()
+            else res.status(404).end()
+        })
+        .catch((error) => next(error));
 });
 
 app.use(unknownEndpoint);
+
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));

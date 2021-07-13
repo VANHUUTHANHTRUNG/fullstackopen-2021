b15a6924151a15672c327f2b69b72435e2383de1
trung <vanhuuthanhtrung@gmail.com>
2021-07-13 16:54:04 +0300

    create submodule for part 4 exercises

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,98 +1,105 @@
-import React, { useState, useEffect } from "react";
-import PersonForm from "./components/PersonForm";
-import PersonsView from "./components/PersonsView";
-import Filter from "./components/Filter";
-import Persons from "./services/Persons";
-import Message from "./components/Message";
+import React, { useState, useEffect } from 'react'
+import PersonForm from './components/PersonForm'
+import PersonsView from './components/PersonsView'
+import Filter from './components/Filter'
+import Persons from './services/Persons'
+import Message from './components/Message'
 const App = () => {
-    const [persons, setPersons] = useState([]);
-    useEffect(() => {
-        Persons.getAll().then((initialPersons) => {
-            setPersons(initialPersons);
-        });
-    }, []);
-    const [filter, setFilter] = useState("");
-    const [message, setMessage] = useState({});
-    const handleSubmit = (submittedPerson) => {
-        const { name } = submittedPerson;
-        const foundPerson = persons.find((person) => person.name === name);
-        if (foundPerson !== undefined) {
-            if (
-                window.confirm(
-                    `${name} is already added to phonebook, update a new number?`
-                )
-            ) {
-                Persons.updatePerson(foundPerson.id, submittedPerson).then(
-                    (updatedPerson) => {
-                        setPersons(
-                            persons.map((person) =>
-                                person.name !== updatedPerson.name
-                                    ? person
-                                    : updatedPerson
-                            )
-                        );
-                        setMessage({
-                            flag: "success",
-                            text: `Modified ${updatedPerson.name}'s info`,
-                        });
-                    }
-                );
-            }
-        } else {
-            Persons.addPerson(submittedPerson).then((addedPerson) => {
-                setPersons([...persons, addedPerson]);
-                setMessage({
-                    flag: "success",
-                    text: `Added ${addedPerson.name}`,
-                });
-            });
-        }
-    };
+  const [persons, setPersons] = useState([])
+  useEffect(() => {
+    Persons.getAll().then((initialPersons) => {
+      setPersons(initialPersons)
+    })
+  }, [])
+  const [filter, setFilter] = useState('')
+  const [message, setMessage] = useState({})
+  const handleSubmit = (submittedPerson) => {
+    const { name } = submittedPerson
+    const foundPerson = persons.find((person) => person.name === name)
+    if (foundPerson !== undefined) {
+      if (
+        window.confirm(
+          `${name} is already added to phonebook, update a new number?`
+        )
+      ) {
+        Persons.updatePerson(foundPerson.id, submittedPerson)
+          .then((updatedPerson) => {
+            setPersons(
+              persons.map((person) =>
+                person.name !== updatedPerson.name ? person : updatedPerson
+              )
+            )
+            setMessage({
+              flag: 'success',
+              text: `Modified ${updatedPerson.name}'s info`,
+            })
+          })
+          .catch((error) => {
+            setMessage({
+              flag: 'error',
+              text: `Error: ${error.response.data.error}`,
+            })
+          })
+      }
+    } else {
+      Persons.addPerson(submittedPerson)
+        .then((addedPerson) => {
+          setPersons([...persons, addedPerson])
+          setMessage({
+            flag: 'success',
+            text: `Added ${addedPerson.name}`,
+          })
+        })
+        .catch((error) => {
+          setMessage({
+            flag: 'error',
+            text: `Error: ${error.response.data.error}`,
+          })
+        })
+    }
+  }
 
-    const handleRemove = (removedPerson) => {
-        const removedPersonID = persons.find(
-            (person) => person === removedPerson
-        ).id;
+  const handleRemove = (removedPerson) => {
+    const removedPersonID = persons.find(
+      (person) => person === removedPerson
+    ).id
 
-        if (window.confirm(`Delete ${removedPerson.name} ?`))
-            Persons.removePerson(removedPersonID)
-                .then(() =>
-                    Persons.getAll().then((currentPersons) => {
-                        setPersons(currentPersons);
-                        setMessage({
-                            flag: "success",
-                            text: `Removed ${removedPerson.name}`,
-                        });
-                    })
-                )
-                .catch((error) => {
-                    console.log(error);
-                    setMessage({
-                        flag: "error",
-                        text: `Information of ${removedPerson.name} has been removed from server`,
-                    });
-                });
-    };
+    if (window.confirm(`Delete ${removedPerson.name} ?`))
+      Persons.removePerson(removedPersonID)
+        .then(() =>
+          Persons.getAll().then((currentPersons) => {
+            setPersons(currentPersons)
+            setMessage({
+              flag: 'success',
+              text: `Removed ${removedPerson.name}`,
+            })
+          })
+        )
+        .catch((error) => {
+          console.log(error)
+          setMessage({
+            flag: 'error',
+            text: `Information of ${removedPerson.name} has been removed from server`,
+          })
+        })
+  }
 
-    const displayedPersons = persons.filter((person) =>
-        person.name.toLowerCase().includes(filter.toLowerCase().trim())
-    );
+  const displayedPersons = persons.filter((person) =>
+    person.name.toLowerCase().includes(filter.toLowerCase().trim())
+  )
 
-    return (
-        <div>
-            <h2>Phonebook</h2>
-            <Message flag={message.flag} text={message.text} />
-            <Filter filter={filter} handleFilter={setFilter} />
-            <PersonForm handleSubmit={handleSubmit} />
-            <h2>Numbers</h2>
-            <PersonsView
-                persons={displayedPersons}
-                handleRemove={handleRemove}
-            />
-        </div>
-    );
-};
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <Message flag={message.flag} text={message.text} />
+      <Filter filter={filter} handleFilter={setFilter} />
+      <PersonForm handleSubmit={handleSubmit} />
+      <h2>Numbers</h2>
+      <PersonsView persons={displayedPersons} handleRemove={handleRemove} />
+    </div>
+  )
+}
 
-export default App;
+export default App
 
 // Create services folder for persons handling, add getAll, create, replace method

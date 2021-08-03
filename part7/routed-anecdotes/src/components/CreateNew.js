88857e6869219b4003dd2f28baf79e1a42e86dde
trung <vanhuuthanhtrung@gmail.com>
2021-08-03 12:30:09 +0300

    fix createnew

--- part7/routed-anecdotes/src/components/CreateNew.js
+++ part7/routed-anecdotes/src/components/CreateNew.js
@@ -1,60 +1,49 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { useHistory } from 'react-router'
+import { useField } from '../hooks/index'
 
 const CreateNew = (props) => {
-  const [content, setContent] = useState('')
-  const [author, setAuthor] = useState('')
-  const [info, setInfo] = useState('')
+  const content = useField('text')
+  const author = useField('text')
+  const info = useField('text')
 
   const history = useHistory()
-
-  const resetInputs = () => {
-    setContent('')
-    setAuthor('')
-    setInfo('')
-  }
 
   const handleSubmit = (e) => {
     e.preventDefault()
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0,
     })
-    resetInputs()
     history.push('/')
+  }
+
+  const handleReset = () => {
+    content.onReset()
+    author.onReset()
+    info.onReset()
   }
 
   return (
     <div>
       <h2>create a new anecdote</h2>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit} onReset={handleReset}>
         <div>
           content
-          <input
-            name='content'
-            value={content}
-            onChange={(e) => setContent(e.target.value)}
-          />
+          <input name='content' {...content} />
         </div>
         <div>
           author
-          <input
-            name='author'
-            value={author}
-            onChange={(e) => setAuthor(e.target.value)}
-          />
+          <input name='author' {...author} />
         </div>
         <div>
           url for more info
-          <input
-            name='info'
-            value={info}
-            onChange={(e) => setInfo(e.target.value)}
-          />
+          <input name='info' {...info} />
         </div>
-        <button>create</button>
+        <button type='submit'>create</button>
+        <button type='reset'>reset</button>
       </form>
     </div>
   )

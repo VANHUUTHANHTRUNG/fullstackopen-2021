8d5056b1db6c050c4283419a1372cb5fa33884d4
trung <vanhuuthanhtrung@gmail.com>
2021-08-03 08:09:20 +0300

    7.1 and 7.2

---
+++ part7/routed-anecdotes/src/components/Anecdote.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Anecdote = ({ anecdote }) => {
+  const { content, author, votes, info } = anecdote
+  console.log(anecdote)
+  return (
+    <div>
+      <h3>
+        ${content} by ${author}
+      </h3>
+      <p>has ${votes} votes</p>
+      <p>
+        for more info see <a href={info}>${info}</a>
+      </p>
+    </div>
+  )
+}
+
+export default Anecdote

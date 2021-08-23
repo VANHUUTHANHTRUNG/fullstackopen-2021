18a8652d7412d4db246ae4778f4b8e744a0b7ed2
trung <vanhuuthanhtrung@gmail.com>
2021-08-23 13:50:37 +0300

    init patentor

---
+++ part9/patientor/backend/src/index.ts
@@ -0,0 +1,14 @@
+import express, { Request, Response } from 'express';
+const app = express();
+app.use(express.json());
+
+const PORT = 3001;
+
+app.get('/api/ping', (_req: Request, res: Response) => {
+  console.log('someone pinged here');
+  res.send('pong');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
